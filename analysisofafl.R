379cba372464eacf5b7278ac1b53c9619af0691a
Mark Richards <Mark.Richards@reece.com.au>
2020-01-08 17:46:58 +1100

    created dataframe to get match IDs and fantasy summed scores

--- analysisofafl.R
+++ analysisofafl.R
@@ -11,23 +11,25 @@ library(tidyr)
 library(dplyr)
 library(ggplot2)
 
-
-main.page <- read_html(x="http://www.footywire.com/afl/footy/ft_match_list?year=2019")
+main.page<-read_html(x="http://www.footywire.com/afl/footy/ft_match_list?year=2019")
 urls<-main.page %>% ###get main page then we get links from main page
   html_nodes(".data:nth-child(5) a")%>%
   html_attr("href") #extract the urls
-head(urls)
-
 scores<-main.page%>%
   html_nodes(".data:nth-child(5) a")%>%
-  html_text() ## gets us the scores
-head(scores)
-
-matchstats<-data.frame(home_team=home_team, away_team=away_team, scores=scores,urls=urls,stringsAsFactors = FALSE)
+  html_text()
+team.names<-main.page%>%
+  html_nodes(".data:nth-child(2)")%>%
+  html_text()
+team.names
+matchstats<-data.frame(team.names=team.names,scores=scores,urls=urls,stringsAsFactors = FALSE)
 head(matchstats)
-
+head(matchstats)
 x1<- matchstats%>%
   separate(urls,c("urls","ID"),sep="=")
+x2<-x1%>%separate(team.names,c("Home","Away"),sep="\nv")
+x3<-x2%>%separate(scores,c("home.score","away.score"),sep="-")
+head(x3) ##looking at our final ID set
 head(x1)
 default.url <-  "http://www.footywire.com/afl/footy/ft_match_statistics?mid="
 basic  <-  data.frame()
@@ -37,8 +39,8 @@ for (i in x1$ID) {
   sel.url      <-  paste(default.url, i, sep="") ##paste forms the url, try a test case when i=2999 and run and see what happens
   htmlcode     <- readLines(sel.url) ###in the same test case type htmlcode hit enter
   export.table <- readHTMLTable(htmlcode) ##like the example before, gets all the tables
-  top.table    <- as.data.frame(export.table[13]) ##looking at tables, 13 is the top one
-  bot.table    <- as.data.frame(export.table[17]) ## 17 is the bottom table
+  top.table    <- as.data.frame(export.table[12]) ##looking at tables, 13 is the top one
+  bot.table    <- as.data.frame(export.table[16]) ## 17 is the bottom table
   ind.table    <- rbind(top.table, bot.table) ##rbind, binds the top table to the bottom table
   ind.table$MatchId <- rep(i, nrow(ind.table)) ##this is adding a match ID which is the unique end of the url
   print(summary(ind.table))
@@ -46,8 +48,3 @@ for (i in x1$ID) {
 }
 basic
 
-
-urls<-main.page %>% ###get main page then we get links from main page
-  html_nodes(".data:nth-child(5) a")%>%
-  html_attr("href") #extract the urls
-head(urls)
--- scrape_footywire.py
+++ scrape_footywire.py
@@ -1,1 +1,72 @@
+from bs4 import BeautifulSoup
 import requests
+import pandas as pd
+
+url = "https://www.footywire.com/afl/footy/ft_match_list?year=2019"
+res = res = requests.get(url)
+soup = BeautifulSoup(res.text, features='lxml')
+tables = soup.select(".data")
+
+dates = []
+teams = []
+venues = []
+crowds = []
+results = []
+disposals = []
+goals = []
+
+counter = 0
+
+for cell in tables:
+    if counter == 0:
+        dates.append(cell.text)
+    elif counter == 1:
+        teams.append(cell.text)
+    elif counter == 2:
+        venues.append(cell.text)
+    elif counter == 3:
+        crowds.append(cell.text)
+    elif counter == 4:
+        results.append(cell.text)
+    elif counter == 5:
+        disposals.append(cell.text)
+    elif counter == 6:
+        goals.append(cell.text)
+    counter += 1
+    if counter == 7:
+        counter = 0
+
+df = pd.DataFrame(
+    list(
+        zip(
+            dates,
+            teams,
+            venues,
+            crowds,
+            results,
+            disposals,
+            goals,
+        )
+    ),
+    columns=[
+        'date',
+        'teams',
+        'venue',
+        'crowd',
+        'results',
+        'disposals',
+        'goals'
+    ])
+
+split_teams = df["teams"].str.split("\nv", n = 1, expand = True)
+df["home_team"]= split_teams[0]
+df["away_team"]= split_teams[1]
+split_results = df["results"].str.split("-", n = 1, expand = True)
+df["home_team_score"]= split_results[0]
+df["away_team_score"]= split_results[1]
+df.drop(columns =["teams", "results", "disposals", "goals"], inplace = True)
+
+df['home_team'] = df['home_team'].str.replace('\n', '')
+df['away_team'] = df['away_team'].str.replace('\n', '')
+print(df)
+
